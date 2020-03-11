ff6a555c9206e162bc2852b0bdd9320a5eee97bd
Julio Oliveira <jcalvesoliveira@hotmail.com>
2020-03-11 19:54:34 -0300

    aula 2

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from textblob import TextBlob
 
-app = Flask('meu_app')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Minha primeira API."
 
-app.run()
+@app.route('/sentimento/<frase>')
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(to='en')
+    polaridade = tb_en.sentiment.polarity
+    return "polaridade: {}".format(polaridade)
+
+app.run(debug=True)
