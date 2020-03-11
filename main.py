1b15c2bbdf8577c1b3104a7a7d424be31eab7e52
Julio Oliveira <jcalvesoliveira@hotmail.com>
2020-03-11 20:47:01 -0300

    aula-3

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from flask import Flask
 from textblob import TextBlob
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('casas.csv')
+colunas = ['tamanho','preco']
+df = df[colunas]
+
+X = df.drop('preco', axis=1)
+y = df['preco']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42)
+modelo = LinearRegression()
+modelo.fit(X_train, y_train)
 
 app = Flask(__name__)
 
@@ -13,5 +28,10 @@ def sentimento(frase):
     tb_en = tb.translate(to='en')
     polaridade = tb_en.sentiment.polarity
     return "polaridade: {}".format(polaridade)
+
+@app.route('/cotacao/<int:tamanho>')
+def cotacao(tamanho):
+    preco = modelo.predict([[tamanho]])
+    return str(preco)
 
 app.run(debug=True)
