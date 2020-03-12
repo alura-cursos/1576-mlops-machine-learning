c04c715bc71d3157742e7af59501df3d9add26da
Julio Oliveira <jcalvesoliveira@hotmail.com>
2020-03-11 22:34:55 -0300

    aula-4

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
 from textblob import TextBlob
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import pickle
 
-df = pd.read_csv('casas.csv')
-colunas = ['tamanho','preco']
-df = df[colunas]
-
-X = df.drop('preco', axis=1)
-y = df['preco']
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42)
-modelo = LinearRegression()
-modelo.fit(X_train, y_train)
+colunas = ['tamanho','ano','garagem']
+modelo = pickle.load(open('modelo.sav','rb'))
 
 app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'julio'
+app.config['BASIC_AUTH_PASSWORD'] = 'alura'
+
+basic_auth = BasicAuth(app)
 
 @app.route('/')
 def home():
     return "Minha primeira API."
 
 @app.route('/sentimento/<frase>')
+@basic_auth.required
 def sentimento(frase):
     tb = TextBlob(frase)
     tb_en = tb.translate(to='en')
     polaridade = tb_en.sentiment.polarity
     return "polaridade: {}".format(polaridade)
 
-@app.route('/cotacao/<int:tamanho>')
-def cotacao(tamanho):
-    preco = modelo.predict([[tamanho]])
-    return str(preco)
+@app.route('/cotacao/', methods=['POST'])
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    preco = modelo.predict([dados_input])
+    return jsonify(preco=preco[0])
 
 app.run(debug=True)
